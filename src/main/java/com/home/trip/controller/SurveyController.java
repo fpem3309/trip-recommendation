8110dd66b8e6adf7f03973205013bc57c3d26138
fpem3309 <fpem3309@gmail.com>
2025-09-22 20:23:23 +0900

    docs: User, Survey API 문서 작성

--- src/main/java/com/home/trip/controller/SurveyController.java
+++ src/main/java/com/home/trip/controller/SurveyController.java
@@ -1,15 +1,19 @@
 package com.home.trip.controller;
 import com.home.trip.domain.dto.SurveyDto;
 import com.home.trip.service.SurveyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "설문", description = "설문 관련 API")
 @RestController
 @RequestMapping("/api-survey")
 @RequiredArgsConstructor
 public class SurveyController {
     private final SurveyService surveyService;
 
+    @Operation(summary = "설문 등록", description = "작성한 설문을 등록")
     @PostMapping("/survey")
     public void saveSurvey(@RequestBody SurveyDto surveyDto) {
         surveyService.save(surveyDto);
--- src/main/java/com/home/trip/controller/UserController.java
+++ src/main/java/com/home/trip/controller/UserController.java
@@ -2,12 +2,15 @@ package com.home.trip.controller;
 
 import com.home.trip.domain.dto.UserDto;
 import com.home.trip.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "회원", description = "회원 관련 API")
 @RestController
 @RequestMapping("/api-user")
 @RequiredArgsConstructor
@@ -15,6 +18,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/user")
+    @Operation(summary = "회원가입", description = "새로운 회원을 등록")
     public void joinUser(@RequestBody UserDto userDto) {
         userService.join(userDto);
     }
