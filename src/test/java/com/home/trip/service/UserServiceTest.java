149810c3ced6a960794ef823b3173ba0855709a2
fpem3309 <fpem3309@gmail.com>
2025-10-09 22:38:01 +0900

    test: Add join unit test
    - UserDto 기반 회원가입 시 DB에 정상 저장되는지 검증
    - 가입 후 findByUserId()로 조회 및 필드 일치 여부 확인
    - @Transactional 적용으로 테스트 후 롤백 보장

---
+++ src/test/java/com/home/trip/service/UserServiceTest.java
@@ -0,0 +1,40 @@
+package com.home.trip.service;
+
+import com.home.trip.domain.User;
+import com.home.trip.domain.dto.UserDto;
+import com.home.trip.domain.enums.Role;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class UserServiceTest {
+
+    @Autowired
+    UserService userService;
+
+
+    @Test
+    void 회원가입() throws Exception {
+        // given
+        UserDto userDto = new UserDto();
+        userDto.setUserId("member1");
+        userDto.setPassword("123");
+        userDto.setEmail("member1@member.com");
+        userDto.setNickname("member");
+        userDto.setRole(Role.USER);
+        userDto.setOauthProvider(null);
+
+        // when
+        userService.join(userDto);
+
+        // then
+        User findUser = userService.findByUserId(userDto.getUserId());
+        Assertions.assertThat(findUser.getUserId()).isEqualTo(userDto.getUserId());
+    }
+}
