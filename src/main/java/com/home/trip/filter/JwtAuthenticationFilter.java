ef53ea1464e3d6541f7ceaa307289fd494956dd1
fpem3309 <fpem3309@gmail.com>
2025-11-29 16:05:20 +0900

    refactor: Extract method with setAuthentication
    - 중복 코드 함수화
    - 한번만 쓰는 변수 삭제

--- src/main/java/com/home/trip/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/home/trip/filter/JwtAuthenticationFilter.java
@@ -12,8 +12,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -33,23 +31,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        try {
-            String accessToken = resolveToken(request);
+        String accessToken = resolveToken(request);
 
-            if (accessToken != null && jwtUtil.validateToken(accessToken)) {
+        try {
+            if (accessToken != null && jwtUtil.validateToken(accessToken)) { // accessToken 검증
                 log.info("===== Valid accessToken =====\n: {}", accessToken);
                 Claims claims = jwtUtil.getClaimsFromToken(accessToken);
-                String username = claims.getSubject();
-                String role = (String) claims.get("role");
-
-                List<GrantedAuthority> authorities = Stream.of(role.split(","))
-                        .map(SimpleGrantedAuthority::new)
-                        .collect(Collectors.toList());
-
-                UsernamePasswordAuthenticationToken auth =
-                        new UsernamePasswordAuthenticationToken(username, null, authorities); // 권한 목록도 추가
-
-                SecurityContextHolder.getContext().setAuthentication(auth); // ⭐️SecurityContextHolder에 사용자 인증 정보 등록
+                setAuthentication(claims.getSubject(), (String) claims.get("role"));
             } else { // guest token 생성
                 String guestToken = jwtUtil.createGuestToken();
                 response.setHeader("X-Guest-Token", guestToken);
@@ -57,26 +45,20 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         } catch (ExpiredJwtException e) { // ⭐️만료된 액세스 토큰 재발급
             String userId = e.getClaims().getSubject();
-
             String refreshToken = resolveRefreshToken(request); // cookie에서 가져옴
             String savedRefreshToken = refreshTokenService.getRefreshToken(userId); // redis에서 가져옴
+
             log.info("===== Expired accessToken =====\nuserId: {}\nrefreshToken: {}\nsavedRefreshToken: {}\nequals: {}\nvalidate: {}\n",
                     userId, refreshToken, savedRefreshToken, refreshToken.equals(savedRefreshToken), jwtUtil.validateToken(refreshToken));
 
             if (refreshToken.equals(savedRefreshToken) && jwtUtil.validateToken(refreshToken)) {
                 String role = e.getClaims().get("role", String.class);
-
-                List<GrantedAuthority> authorities = Stream.of(role.split(","))
-                        .map(SimpleGrantedAuthority::new)
-                        .collect(Collectors.toList());
-
                 String newAccessToken = jwtUtil.generateAccessToken(userId, role);// 새 AccessToken 발급
+
                 log.info("===== newAccessToken=====\n: {}", newAccessToken);
 
                 response.setHeader("Authorization", "Bearer " + newAccessToken);
-
-                Authentication auth = new UsernamePasswordAuthenticationToken(userId, null, authorities);
-                SecurityContextHolder.getContext().setAuthentication(auth); // ⭐️ 새 인증 정보 등록
+                setAuthentication(userId, role);
             } else {
                 log.warn("Invalid or expired refresh token for user: {}", userId);
             }
@@ -88,6 +70,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
+    /**
+     * header에서 accessToken 가져오기
+     *
+     * @param request
+     * @return
+     */
     private String resolveToken(HttpServletRequest request) {
         String header = request.getHeader("Authorization");
         if (header != null && header.startsWith("Bearer ")) {
@@ -96,6 +84,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         return null;
     }
 
+    /**
+     * cookie에서 refreshToken 가져오기
+     *
+     * @param request
+     * @return
+     */
     private String resolveRefreshToken(HttpServletRequest request) {
         Cookie[] cookies = request.getCookies();
         if (cookies == null) return null;
@@ -106,5 +100,21 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             }
         }
         return null;
+    }
+
+    /**
+     * 권한 GrantedAuthority List로 리턴
+     *
+     * @param role
+     * @return
+     */
+    private static void setAuthentication(String userId, String role) {
+        List<SimpleGrantedAuthority> authorities = Stream.of(role.split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+
+        UsernamePasswordAuthenticationToken auth =
+                new UsernamePasswordAuthenticationToken(userId, null, authorities); // ID, 권한 목록 추가
+        SecurityContextHolder.getContext().setAuthentication(auth); // ⭐️SecurityContextHolder에 사용자 인증 정보 등록
     }
 }
