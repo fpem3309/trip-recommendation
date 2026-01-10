d5d4ef8e268bddc469d733c1f519b95facef52c5
fpem3309 <fpem3309@gmail.com>
2026-01-10 17:47:21 +0900

    fix: Add surveyId field to RecommendResponseDto
    - surveyId로 검색 가능하게 surveyId 추가해

--- src/main/java/com/home/trip/domain/dto/RecommendResponseDto.java
+++ src/main/java/com/home/trip/domain/dto/RecommendResponseDto.java
@@ -4,13 +4,14 @@ import com.home.trip.domain.TripRecommendation;
 import lombok.Builder;
 
 @Builder
-public record RecommendResponseDto(Long id, String bestSeason, String city, String country,
-                                   String transportation, String estimatedBudget, String period,
+public record RecommendResponseDto(Long surveyId, Long tripRecommendationId, String bestSeason, String city,
+                                   String country, String transportation, String estimatedBudget, String period,
                                    String recommendation) {
 
     public static RecommendResponseDto createRecommendDto(TripRecommendation recommendation) {
         return RecommendResponseDto.builder()
-                .id(recommendation.getId())
+                .surveyId(recommendation.getSurvey().getId())
+                .tripRecommendationId(recommendation.getId())
                 .bestSeason(recommendation.getBestSeason())
                 .city(recommendation.getCity())
                 .country(recommendation.getCountry())
